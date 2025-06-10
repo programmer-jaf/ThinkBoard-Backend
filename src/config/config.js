c1b682df91738abe2ec4489ba3357ca4fadb658d
Programmer-Jaf <programmerjaf@gmail.com>
2025-06-10 16:44:57 +0600

    Add configuration file for environment variables

---
+++ src/config/config.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+import dotenv from 'dotenv'
+dotenv.config();
+
+// export the environment-variables
+const _config = {
+  PORT: process.env.PORT,
+  CORS_ORIGIN: process.env.CORS_ORIGIN,
+  MONGODB_URI: process.env.MONGODB_URI,
+  JWT_SECRET: process.env.ACCESS_TOKEN_SECRET,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
+  NODE_ENV: process.env.NODE_ENV
+}
+
+const config = Object.freeze(_config)
+export default config;
